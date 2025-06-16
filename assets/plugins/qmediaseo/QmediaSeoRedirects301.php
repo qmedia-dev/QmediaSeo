c91cc26518f111bd361bc4bdcdb47c161fa7c874
hkyss <hkyss.services@protonmail.com>
2025-06-16 13:08:06 +0300

    fix: redirects

--- assets/plugins/qmediaseo/QmediaSeoRedirects301.php
+++ assets/plugins/qmediaseo/QmediaSeoRedirects301.php
@@ -4,36 +4,30 @@ if (!defined('MODX_BASE_PATH')) {
     die('What are you doing? Get out of here!');
 }
 
-// Получаем содержимое списка редиректов из настроек сайта
 $clientsettingsList = evo()->getConfig('client_seo_redirects_301') ?? '';
-
-// Получаем содержимое списка редиректов из чанка
 $chunkList = evo()->getChunk('seo_redirects_301') ?? '';
-
-//Разбиваем код чанка по переводу строки в массив правил редиректа
 $mapLines = explode("\n", $clientsettingsList . "\n" . $chunkList);
 
-// Создаем пустой массив
 $mapArr = array();
 
-// Проходимся по массиву со строками правил редиректа
 foreach ($mapLines as $line) {
-    if (empty($line)) {
+    if (empty($line) || strpos($line, '||') === false) {
         continue;
     }
 
-    list($oldLink, $newLink) = explode('||', $line);
-    $oldLink = trim($oldLink);
-    $newLink = trim($newLink);
-    if (!empty($oldLink) && !empty($newLink) && $oldLink !== $newLink) {
-        $mapArr[$oldLink] = $newLink;
+    $parts = explode('||', $line);
+    if (count($parts) === 2) {
+        $oldLink = trim($parts[0]);
+        $newLink = trim($parts[1]);
+
+        if (!empty($oldLink) && !empty($newLink)) {
+            $mapArr[$oldLink] = $newLink;
+        }
     }
 }
 
-// Получаем запрошенный адрес
 $query = $_SERVER['REQUEST_URI'];
 
-// Если запрошенный адрес есть в правилах редиректа, то осуществляем редирект
 if (isset($mapArr[$query])) {
     evo()->sendRedirect($mapArr[$query], 0, 'REDIRECT_HEADER', 'HTTP/1.1 301 Moved Permanently');
     exit();
